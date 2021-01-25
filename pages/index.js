ce422396c7d6362ad21b225b899ef3f1bb9951e0
Renato Castro <renatocastro02@hotmail.com>
2021-01-25 15:11:29 -0300

    fix: change GitHubCorner projectUrl

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/RenatoDTH" />
+      <GitHubCorner projectUrl="https://github.com/RenatoDTH/Allura-Quiz" />
     </QuizBackground>
   );
 }
