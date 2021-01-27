ead2fb70e14b66b8df9dcb15a2aef23443549c6e
Renato Castro <renatocastro02@hotmail.com>
2021-01-27 13:31:23 -0300

    refactor: move QuizContainer to own file

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useCallback, useState } from 'react';
 import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
@@ -17,17 +17,6 @@ import Button from '../src/components/Button';
 //   background-size: cover;
 //   background-position: center;
 // `;
-
-const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
