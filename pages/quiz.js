11e93f5c83eaee82bd28409edb2db60b12844cc6
Renato Castro <renatocastro02@hotmail.com>
2021-01-27 16:39:02 -0300

    feat: build quiz page

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,3 +1,116 @@
+import { useCallback, useEffect, useState } from 'react';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import Button from '../src/components/Button';
+
+const LoadingWidget = () => {
+  return (
+    <Widget>
+      <Widget.Header>Carregando...</Widget.Header>
+      <Widget.Content>[Desafio do Loading]</Widget.Content>
+    </Widget>
+  );
+};
+
+const QuestionWidget = ({
+  question,
+  totalQuestions,
+  questionIndex,
+  onSubmit,
+}) => {
+  const questionId = `question__${questionIndex}`;
+
+  const handleSubmitInfo = useCallback(
+    (event) => {
+      event.preventDefault();
+      onSubmit();
+    },
+    [onSubmit],
+  );
+  return (
+    <Widget>
+      <Widget.Header>
+        {/* <BackLinkArrow href="/" /> */}
+        <h3>{`Pergunta ${questionIndex + 1} de ${totalQuestions}`}</h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{ width: '100%', height: '150px', objectFit: 'cover' }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>{question.title}</h2>
+        <p>{question.description}</p>
+
+        <form onSubmit={handleSubmitInfo}>
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+                key={alternativeId}
+              >
+                <input id={alternativeId} type="radio" name={questionId} />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+          <Button type="submit">Confirmar</Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+};
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+
 export default function QuizPage() {
-  return <div>Página de quiz</div>;
+  const [screenState, setScreenState] = useState(screenStates.LOADING);
+  const totalQuestions = db.questions.length;
+  const [questionIndex, setQuestionIndex] = useState([0]);
+  const question = db.questions[questionIndex];
+
+  const handleSubmitQuiz = useCallback(() => {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setQuestionIndex(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }, [questionIndex, totalQuestions]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  }, []);
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            totalQuestions={totalQuestions}
+            questionIndex={questionIndex}
+            onSubmit={handleSubmitQuiz}
+          />
+        )}
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+        {screenState === screenStates.RESULT && (
+          <div>Você acertou X questões, parabéns!</div>
+        )}
+      </QuizContainer>
+    </QuizBackground>
+  );
 }
