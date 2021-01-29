7d7eac086933eba5604bb105475a0a63f201c9d5
Renato Castro <renatocastro02@hotmail.com>
2021-01-29 15:50:19 -0300

    feat: connection to external link

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,7 +1,41 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
 
-const QuizDaGaleraPage = () => {
-  return <div>Desafio da próxima aula junto com as animações</div>;
+const QuizDaGaleraPage = ({ dbExternal }) => {
+  return (
+    <ThemeProvider theme={dbExternal.theme}>
+      <QuizScreen
+        externalQuestions={dbExternal.questions}
+        externalBg={dbExternal.bg}
+      />
+    </ThemeProvider>
+  );
 };
 
 export default QuizDaGaleraPage;
+
+export const getServerSideProps = async (context) => {
+  const [projectName, githubUser] = context.query.id.split('___');
+  const dbExternal = await fetch(
+    `https://${projectName}.${githubUser}.vercel.app/api/db`,
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error('Falha em pegar os dados');
+    })
+    .then((resObject) => {
+      return resObject;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  return {
+    props: {
+      dbExternal,
+    },
+  };
+};
