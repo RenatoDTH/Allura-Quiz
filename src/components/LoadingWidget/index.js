9da50ba188808ed891f5af8a6a8d89b322aec30c
Renato Castro <renatocastro02@hotmail.com>
2021-01-31 13:19:42 -0300

    refactor: move LoadingWidget to own file

---
+++ src/components/LoadingWidget/index.js
@@ -0,0 +1,25 @@
+import { Lottie } from '@crello/react-lottie';
+import Widget from '../Widget';
+import loadingAnimation from '../../screens/Quiz/animations/loading.json';
+
+const LoadingWidget = () => {
+  return (
+    <Widget>
+      <Widget.Header>Carregando...</Widget.Header>
+      <Widget.Content style={{ display: 'flex', justifyContent: 'center' }}>
+        <Lottie
+          width="200px"
+          height="200px"
+          className="lottie-container basic"
+          config={{
+            animationData: loadingAnimation,
+            loop: true,
+            autoplay: true,
+          }}
+        />
+      </Widget.Content>
+    </Widget>
+  );
+};
+
+export default LoadingWidget;
