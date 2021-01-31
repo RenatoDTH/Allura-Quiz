db4fd0d24f304e94c7b5a9da35cd2ae3c3c201ec
Renato Castro <renatocastro02@hotmail.com>
2021-01-31 12:57:13 -0300

    refactor: change globalStyle to own file

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,7 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import db from '../db.json';
-
-const GlobalStyle = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    margin: 0;
-    padding: 0;
-    /* New styles */
-    display: flex;
-    flex-direction: column;
-    font-family: 'Lato', sans-serif;
-    // Deixa branco no começo
-    color: ${({ theme }) => theme.colors.contrastText};
-  }
-  html, body {
-    min-height: 100vh;
-  }
-  #__next {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-  }
-`;
+import GlobalStyle from '../src/styles';
 
 const { theme } = db;
 
